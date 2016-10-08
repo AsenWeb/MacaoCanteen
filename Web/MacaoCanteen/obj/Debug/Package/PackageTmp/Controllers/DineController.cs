3958af80abcb506105224970daf2a0c1504e473f
AsenLins <736239001@qq.com>
2016-10-08 17:58:23 +0800

    v0.2

---
+++ Web/MacaoCanteen/obj/Debug/Package/PackageTmp/Controllers/DineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacaoCanteen.Controllers
+{
+    public class DineController : Controller
+    {
+        //
+        // GET: /Dine/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
